12f222be59a93241c6df35fdf11800b625181d6b
Rohit Sharma <rs@gmail.com>
2017-06-04 02:42:37 -0400

    Initial commit

---
+++ src/com/org/sample/Main.java
@@ -0,0 +1,27 @@
+package com.org.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> namesList = new ArrayList<String>();
+        namesList.add("David");
+        namesList.add("Mike");
+        namesList.add("John");
+
+        lambdaToPrintItemsInCollection1(namesList);
+        lambdaToPrintItemsInCollection2(namesList);
+    }
+
+    private static void lambdaToPrintItemsInCollection2(List<String> list) {
+        System.out.println("Usage 1: Print using lambda");
+        list.forEach(name -> System.out.println(name));
+    }
+
+    private static void lambdaToPrintItemsInCollection1(List<String> list) {
+        System.out.println("\nUsage 2: Print using lambda");
+        list.forEach(System.out::println);
+    }
+}
