6cb1f754c7637d06d3a22c66084c8ada19593805
Rohit Sharma <rs@gmail.com>
2017-06-15 22:47:20 -0400

    develop commit 5

--- src/com/org/sample/Main.java
+++ src/com/org/sample/Main.java
@@ -15,7 +15,7 @@ public class Main {
         lambdaToPrintItemsInCollection2(namesList);
 
         System.out.println("develop commit 1");
-        System.out.println("develop commit 10");
+        System.out.println("11");
         System.out.println("develop commit 3");
     }
 
